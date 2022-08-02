fa25fbb49c979d7b838f6209d65db8d219970a17
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-02 16:59:49 +0100

    Set app  as class component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
+import React, { PureComponent } from 'react';
 import Calculator from './components/Calculator';
 
-function App() {
-  return (
-    <Calculator />
-  );
+export default class App extends PureComponent {
+  render() {
+    return (
+      <Calculator />
+    );
+  }
 }
-
-export default App;
