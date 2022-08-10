4d21e2fb00b7f030c085fdd4967a6daee185a918
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-10 15:34:15 +0100

    Set the router and navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
-import React, { PureComponent } from 'react';
-import Calculator from './components/Calculator';
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-export default class App extends PureComponent {
-  render() {
-    return (
-      <Calculator />
-    );
-  }
-}
+import Home from './container/Home';
+import Calculator from './components/Calculator';
+import Quote from './container/Quote';
+import Navbar from './components/Navbar';
+
+const App = () => (
+  <div>
+    <Navbar />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<Calculator />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
+  </div>
+
+);
+
+export default App;
---
+++ src/components/Navbar.js
@@ -0,0 +1,54 @@
+import React, { useState } from 'react';
+import './navbar.css';
+
+const Navbar = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
+
+  return (
+    <nav className="navigation">
+      <a href="/" className="brand-name">
+        MacroSoft
+      </a>
+      <button
+        type="button"
+        className="hamburger"
+        onClick={() => {
+          setIsNavExpanded(!isNavExpanded);
+        }}
+      >
+        {/* icon from heroicons.com */}
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          viewBox="0 0 20 20"
+          fill="white"
+        >
+          <path
+            fillRule="evenodd"
+            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM9 15a1 1 0 011-1h6a1 1 0 110 2h-6a1 1 0 01-1-1z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </button>
+      <div
+        className={
+          isNavExpanded ? 'navigation-menu expanded' : 'navigation-menu'
+        }
+      >
+        <ul>
+          <li>
+            <a href="/">Home</a>
+          </li>
+          <li>
+            <a href="/calculator">Calculator</a>
+          </li>
+          <li>
+            <a href="/quote">Quote</a>
+          </li>
+        </ul>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
