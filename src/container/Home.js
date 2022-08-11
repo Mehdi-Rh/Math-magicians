f0d88bfa87495aece0864127da77b032092b1965
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-11 13:04:30 +0100

    Set the full website site

--- src/container/Home.js
+++ src/container/Home.js
@@ -1,7 +1,15 @@
 import React from 'react';
+import './Home.css';
 
 const Home = () => (
-  <h1>Home</h1>
+  <div className="Home">
+    <h1>Welcome to our page!</h1>
+    <p>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit.
+    </p>
+    <p>Esse neque explicabo quod eligendi necessitatibus beatae cumque tenetur.</p>
+    <p>Officiis, excepturi illum eos vel delectus quod? </p>
+  </div>
 );
 
 export default Home;
