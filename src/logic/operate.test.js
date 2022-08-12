779e72a2af77170b07764a5c87bb3ca094f51bf3
Hamma Azarok <hammaagazarok@gmail.com>
2022-08-12 12:16:55 +0200

    add test for operate

--- src/logic/operate.test.js
+++ src/logic/operate.test.js
@@ -5,3 +5,10 @@ describe('Add operation', () => {
     expect(operate(4, 2, '+')).toBe('6');
   });
 });
+
+describe('Substract operation', () => {
+  test('Two positive numbers', () => {
+    expect(operate(8, 4, '-')).toBe('4');
+  });
+});
+
