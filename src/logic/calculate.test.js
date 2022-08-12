36066477a09f5158787d8cd7743a6dc033a421c4
Hamma Azarok <hammaagazarok@gmail.com>
2022-08-12 12:16:41 +0200

    add test for calculate

--- src/logic/calculate.test.js
+++ src/logic/calculate.test.js
@@ -1,6 +1,6 @@
 import calculate from './calculate';
 
-describe('AC button test', () => {
+describe('Calculate test', () => {
   test('AC button', () => {
     const obj = {
       total: null,
@@ -18,4 +18,37 @@ describe('AC button test', () => {
 
     expect(result).toEqual(expected);
   });
+  test('-/+ Button', () => {
+    let obj = calculate({}, '3');
+    obj = calculate(obj, '+');
+    obj = calculate(obj, '3');
+    obj = calculate(obj, '=');
+    obj = calculate(obj, '+/-');
+    const result = obj;
+    const expected = { total: '-6', next: null, operation: null };
+    expect(result).toEqual(expected);
+  });
+  test('Add 2 + 5', () => {
+    let obj = calculate({}, '2');
+    obj = calculate(obj, '+');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    const result = obj;
+    const expected = { total: '7', next: null, operation: null };
+    expect(result).toEqual(expected);
+  }
+  )
+
+  test('2 * 5', () => {
+    let obj = calculate({}, '2');
+    obj = calculate(obj, 'x');
+    obj = calculate(obj, '5');
+    obj = calculate(obj, '=');
+    const result = obj;
+    const expected = { total: '10', next: null, operation: null };
+    expect(result).toEqual(expected);
+  }
+  )
+
+
 });
