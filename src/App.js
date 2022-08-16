5d1e27b3d52ea4eee9bd247aaa5c9488b7c25497
Mehdi-Rh <mehdi.rahl92@gmail.com>
2022-08-16 18:19:44 +0100

    Try netlify without footer

--- src/App.js
+++ src/App.js
@@ -5,19 +5,15 @@ import Home from './container/homePage/Home';
 import CalculatorPage from './container/calculatorPage/CalculatorPage';
 import Quote from './container/quotePage/Quote';
 import Navbar from './components/navbar/Navbar';
-import Footer from './components/footer/Footer';
 
 const App = () => (
   <div>
     <Navbar />
-    <main>
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/calculator" element={<CalculatorPage />} />
-        <Route path="/quote" element={<Quote />} />
-      </Routes>
-    </main>
-    <Footer />
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/calculator" element={<CalculatorPage />} />
+      <Route path="/quote" element={<Quote />} />
+    </Routes>
   </div>
 
 );
